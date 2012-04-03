52be4d5e987d230b58c0d28f07662743dd6abb3b
Matthew Boston <matthew.boston@gmail.com>
2012-04-03 16:53:11 -0400

    refactor core.logic zebra

--- src/zebra.clj
+++ src/zebra.clj
@@ -1,5 +1,5 @@
 (ns zebra
-  (:refer-clojure :exclude [reify == inc])
+  (:refer-clojure :exclude [==])
   (:use [clojure.core.logic]
         [clojure.tools.macro :as macro]))
 
@@ -21,27 +21,25 @@
 (defn owns-zebra [who]
   (macro/symbol-macrolet [_ (lvar)]
     (all
-     (fresh [street]
-            (== [_ _ [_ _ _ 'milk _] _ _] street)
-            (membero ['red 'englishman _ _ _] street)
-            (membero [_ 'spaniard 'dog _ _] street)
-            (membero ['green _ _ 'coffee _] street)
-            (membero [_ 'ukranian _ 'tea _] street)
-            (is-right-of ['green _ _ _ _] ['ivory _ _ _ _] street)
-            (membero [_ _ 'snails _ 'old-gold] street)
-            (membero ['yellow _ _ _ 'kools] street)
-            (firsto street [_ 'norwegian _ _ _])
-            (next-to [_ _ _ _ 'chesterfields] [_ _ 'fox _ _] street)
-            (next-to [_ _ _ _ 'kools] [_ _ 'horse _ _] street)
-            (membero [_ _ _ 'orange-juice 'lucky-strike] street)
-            (membero [_ 'japanese _ _ 'parliaments] street)
-            (next-to [_ 'norwegian _ _ _] ['blue _ _ _ _] street)
-            (membero [_ who 'zebra _ _] street)))))
+      (fresh [street]
+        (== street [_ _ [_ _ _ 'milk _] _ _])
+        (membero ['red 'englishman _ _ _] street)
+        (membero [_ 'spaniard 'dog _ _] street)
+        (membero ['green _ _ 'coffee _] street)
+        (membero [_ 'ukranian _ 'tea _] street)
+        (is-right-of ['green _ _ _ _] ['ivory _ _ _ _] street)
+        (membero [_ _ 'snails _ 'old-gold] street)
+        (membero ['yellow _ _ _ 'kools] street)
+        (firsto street [_ 'norwegian _ _ _])
+        (next-to [_ _ _ _ 'chesterfields] [_ _ 'fox _ _] street)
+        (next-to [_ _ _ _ 'kools] [_ _ 'horse _ _] street)
+        (membero [_ _ _ 'orange-juice 'lucky-strike] street)
+        (membero [_ 'japanese _ _ 'parliaments] street)
+        (next-to [_ 'norwegian _ _ _] ['blue _ _ _ _] street)
+        (membero [_ who 'zebra _ _] street)))))
 
-;; (run* [q]
-;;       (fresh [who street]
-;;              (owns-zebra who)
-;;              (== q who)))
+;; (run 1 [q]
+;;      (owns-zebra q))
 
 ;; ([[yellow norwegian  fox    _.0          kools]
 ;;   [blue   ukranian   horse  tea          chesterfields]
